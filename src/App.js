b9d0ca4a891a796653b5b48fc2bc6e6c06ed7755
Poonam <poonam.chauhan229@gmail.com>
2022-09-27 15:26:29 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
-import logo from './logo.svg';
+import React, { Component } from 'react'
 import './App.css';
+import Header from './components/Header'
+import ProductDispplay from './components/ProductDispplay'
+import JSON from './components/db.json'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+  constructor(){
+    super()
+    this.state={
+      productData:JSON
+    }
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <Header/>
+        <ProductDispplay proddata={this.state.productData}/>
+      </div>
+    )
+  }
 }
-
-export default App;
